4e9e6571e975310856dd16b00664168984ffe08f
shah541-g <syedabdulalishah.786@gmail.com>
2025-07-09 09:24:54 +0500

    first commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export function Card({ children, className }: CardProps) {
   return (
     <div
       className={twMerge(
-        'bg-white dark:bg-gray-800 rounded-lg shadow-lg p-4 backdrop-blur-sm bg-opacity-90 dark:bg-opacity-90',
+        'bg-white dark:bg-gray-400 rounded-lg shadow-lg p-4 backdrop-blur-sm bg-opacity-90 dark:bg-opacity-90',
         className
       )}
     >
